f313c5b3cc4dfbb4bd329225a658f62af86a5a16
Heiner Litz <hlitz@ucsc.edu>
2018-09-03 21:17:15 -0700

    Revert "test2 revert again"
    This reverts commit 7def3a157d6fe1b44850f16a219e7944379ff9ce.

--- drivers/lightnvm/pblk.h
+++ drivers/lightnvm/pblk.h
@@ -619,7 +619,6 @@ struct pblk_rail {
 	unsigned long *busy_bitmap;     /* LUNs currently serving a write/erase */
 	u64 *lba;                       /* parity LBA buffer */
 	struct semaphore *stride_sem;   /* Per stride semaphore */
-  spinlock_t lock;
 };
 
 /* Initialize and tear down RAIL */
