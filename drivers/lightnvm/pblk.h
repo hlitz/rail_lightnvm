c3d655783f0cd53a306b26962f284dcdb768ea7c
Heiner Litz <hlitz@ucsc.edu>
2018-09-05 14:50:58 -0700

    lightnvm: pblk: Add RAIL interface
    In prepartion of supporting RAIL, add the RAIL API.
    
    Signed-off-by: Heiner Litz <hlitz@ucsc.edu>

--- drivers/lightnvm/pblk.h
+++ drivers/lightnvm/pblk.h
@@ -46,7 +46,7 @@
 #define PBLK_COMMAND_TIMEOUT_MS 30000
 
 /* Max 512 LUNs per device */
-#define PBLK_MAX_LUNS_BITMAP (4)
+#define PBLK_MAX_LUNS_BITMAP (512)
 
 #define NR_PHY_IN_LOG (PBLK_EXPOSED_PAGE_SIZE / PBLK_SECTOR)
 
@@ -127,7 +127,7 @@ struct pblk_g_ctx {
 /* partial read context */
 struct pblk_pr_ctx {
 	struct bio *orig_bio;
-	DECLARE_BITMAP(bitmap, NVM_MAX_VLBA);
+	unsigned long *bitmap;
 	unsigned int orig_nr_secs;
 	unsigned int bio_init_idx;
 	void *ppa_ptr;
@@ -346,8 +346,8 @@ enum {
  * recovery.
  */
 
-#define SMETA_VERSION_MAJOR (0)
-#define SMETA_VERSION_MINOR (1)
+#define SMETA_VERSION_MAJOR (1)
+#define SMETA_VERSION_MINOR (0)
 
 #define EMETA_VERSION_MAJOR (0)
 #define EMETA_VERSION_MINOR (2)
@@ -375,7 +375,9 @@ struct line_smeta {
 	/* Active writers */
 	__le32 window_wr_lun;	/* Number of parallel LUNs to write */
 
-	__le32 rsvd[2];
+	u8 rail_stride_width;	/* RAIL configuration of current line */
+
+	u8 rsvd[7];
 
 	__le64 lun_bitmap[];
 };
@@ -490,6 +492,8 @@ struct pblk_line {
 	struct pblk_w_err_gc *w_err_gc;	/* Write error gc recovery metadata */
 
 	spinlock_t lock;		/* Necessary for invalid_bitmap only */
+
+	int rail_stride_width;          /* RAIL stride width for this line */
 };
 
 #define PBLK_DATA_LINES 4
@@ -610,6 +614,44 @@ struct pblk_addrf {
 	int sec_ws_stripe;
 };
 
+struct p2b_entry {
+	int pos;
+	int nr_valid;
+};
+
+struct pblk_rail {
+	struct p2b_entry **p2b;         /* Maps RAIL sectors to rb pos */
+	struct page *pages;             /* Pages to hold parity writes */
+	u16 stride_width;               /* Zero if RAIL is disabled */
+	void **data;                    /* Buffer that holds parity pages */
+	DECLARE_BITMAP(busy_bitmap, PBLK_MAX_LUNS_BITMAP);
+	u64 *lba;                       /* Buffer to compute LBA parity */
+};
+
+/* RAIL configured and enabled? */
+int pblk_rail_stride_width(struct pblk *pblk);
+/* Initialize and tear down RAIL */
+int pblk_rail_init(struct pblk *pblk);
+void pblk_rail_free(struct pblk *pblk);
+/* Recovery Path */
+bool pblk_rail_lun_is_parity(struct pblk *pblk, struct pblk_line *line,
+			     int lun);
+/* Adjust some system parameters */
+bool pblk_rail_meta_distance(struct pblk *pblk, struct pblk_line *data_line);
+/* Core */
+int pblk_rail_down_stride(struct pblk *pblk, int lun, int timeout);
+void pblk_rail_up_stride(struct pblk *pblk, int lun);
+/* Write path */
+int pblk_rail_submit_write(struct pblk *pblk);
+/* Read Path */
+int pblk_rail_read_bio(struct pblk *pblk, struct nvm_rq *rqd, int blba,
+		       unsigned long *read_bitmap, int bio_init_idx,
+		       struct bio **bio);
+/* Mapping path */
+void pblk_rail_map_sec(struct pblk *pblk, struct pblk_line *line, int sentry,
+		       struct pblk_sec_meta *meta_list, __le64 *lba_list,
+		       struct ppa_addr ppa, u64 paddr, int sec, int valid);
+
 struct pblk {
 	struct nvm_tgt_dev *dev;
 	struct gendisk *disk;
@@ -715,6 +757,8 @@ struct pblk {
 	struct timer_list wtimer;
 
 	struct pblk_gc gc;
+
+	struct pblk_rail rail;
 };
 
 struct pblk_line_ws {
@@ -795,6 +839,7 @@ struct nvm_chk_meta *pblk_chunk_get_off(struct pblk *pblk,
 void pblk_log_write_err(struct pblk *pblk, struct nvm_rq *rqd);
 void pblk_log_read_err(struct pblk *pblk, struct nvm_rq *rqd);
 int pblk_submit_io(struct pblk *pblk, struct nvm_rq *rqd);
+int pblk_submit_io_sem(struct pblk *pblk, struct nvm_rq *rqd);
 int pblk_submit_io_sync(struct pblk *pblk, struct nvm_rq *rqd);
 int pblk_submit_io_sync_sem(struct pblk *pblk, struct nvm_rq *rqd);
 int pblk_submit_meta_io(struct pblk *pblk, struct pblk_line *meta_line);
@@ -1107,6 +1152,16 @@ static inline u64 pblk_dev_ppa_to_line_addr(struct pblk *pblk,
 	}
 
 	return paddr;
+}
+
+static inline int pblk_pos_to_lun(struct nvm_geo *geo, int pos)
+{
+	return pos >> ilog2(geo->num_ch);
+}
+
+static inline int pblk_pos_to_chnl(struct nvm_geo *geo, int pos)
+{
+	return pos % geo->num_ch;
 }
 
 static inline struct ppa_addr pblk_ppa32_to_ppa64(struct pblk *pblk, u32 ppa32)
